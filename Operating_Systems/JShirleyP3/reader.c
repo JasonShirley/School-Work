30ada2475ea12ec922ca9e0e59fe33f443ea3762
jasonshirleyr <jshirley@eou.edu>
2018-10-17 19:59:56 -0700

    Add files via upload

--- Operating_Systems/JShirleyP3/reader.c
+++ Operating_Systems/JShirleyP3/reader.c
@@ -2,21 +2,30 @@
 #include <sys/msg.h>
 #include <sys/shm.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 int main() {
-	key_t key = ftok("shmfile", 'r');				//Generate a System V IPC Key for memshare
-	char charArray[100];			//Used to identify shmloc
-	int shmid;
-	char *data;
-	
-	if(key == -1){
-		perror("ftokl");
-		exit(1);
-	}	
-	shmid = shmget(key, 1024, 0644 | IPC_CREAT);
-	data = (char*)shmat(shmid, (void*)0, 0);
-	printf("Written in memory: %s\n", data);
-	shmdt(data);
+		printf("%i ", getpid());
+		printf("shmwriter: enter a shm loc: ");
+
+		key_t key = ftok("shmfile", 'r');				//Generate a System V IPC Key for memshare
+		char *charArray[100];			//Used to identify shmloc
+		int shmid;
+		char *data;
+
+		if(key == -1){
+			perror("ftokl");
+			exit(1);
+		}
+	while(1){
+		shmid = shmget(key, sizeof(char)*100, 0644 | IPC_CREAT);
+		data = (char*)shmat(shmid, (void*)0, 0);
+		printf("%i ", getpid());
+		printf("Written in memory: %s\n", data);
+		shmdt(data);
+		sleep(2);
+	}
 
 	shmctl(shmid,IPC_RMID,NULL);
 }
--- Operating_Systems/JShirleyP3/writer.c
+++ Operating_Systems/JShirleyP3/writer.c
@@ -2,21 +2,23 @@
 #include <sys/msg.h>
 #include <sys/shm.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main() {
 	key_t key = ftok("shmfile", 'r');				//Generate a System V IPC Key for memshare
-	char charArray[100];			//Used to identify shmloc	
-	int shmid;				
-	char *data;	
+	char charArray[100];			//Used to identify shmloc
+	int shmid;
+	char *data;
 
 	if(key == -1){
 		perror("ftokl");
 		exit(1);
 	}
-	shmid = shmget(key, 1024, 0644 | IPC_CREAT);
+	shmid = shmget(key, sizeof(char)*100, 0644 | IPC_CREAT);
 	data = (char*)shmat(shmid, (void*)0, 0);
 	printf("write data: ");
 	fgets(data, 100, stdin);
+	strtok(data, "\n");
 	printf("Data written in memory: %s\n", data);
 
 	shmdt(data);
