c8fdcd8693872ad7ff417ebfe09b4805bbcdfa2d
jasonshirleyr <jshirley@eou.edu>
2018-10-21 14:37:31 -0700

    Add files via upload
    Ready for the presses

--- Operating_Systems/JShirleyP3/reader.c
+++ Operating_Systems/JShirleyP3/reader.c
@@ -1,3 +1,14 @@
+/*
+ * The purpose of this application is to read from a shared memory segment.
+ * This application will connect to a shared memory segment, and read the
+ * contents from it. The user will define the shared memory segment to connect to.
+ * The application will prompt the user to input the shared memory segment, 
+ * and the application will connect to the shared memory segment according to
+ * the input from the user. 
+*/
+
+
+
 #include <stdio.h>
 #include <signal.h>
 #include <sys/msg.h>
@@ -6,9 +17,15 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-char* data;
-int shmid;
+char* data;												// Used to store user input
+int shmid;												// used for identifying shared memory id
 
+/*
+ * handle_signal is used for the signal handling. The signals that will be caught
+ * are SIGINT, for signal interrupt and SIGUSR1 for custom user signal. SIGINT
+ * will detach from the memory segment and close the program. SIGUSR1 will 
+ * simply stat that the user signal was caught and continue on with the program. 
+*/
 void handle_signal(int sig){
 	if(sig == SIGINT){
 		printf("\nshmreader: SIGINT signal caught. exiting \n");
@@ -20,31 +37,27 @@ void handle_signal(int sig){
 		printf("\n caught sigusr1 \n");
 	}
 }
-/*
- * The purpose of this application is to read from a shared memory segment.
- * This application will connect to a shared memory segment, and read the
- * contents from it. The user will define the shared memory segment to connect to.
- * The application will prompt the user to input the shared memory segment, 
- * and the application will connect to the shared memory segment according to
- * the input from the user. 
-*/
 
 
 int main() {
 	void handle_signal(int sig);
 	char charArray[100];									// Used to identify shmloc
 	int charArrToInt;										// Used to convert the charArray to int, for ftok
-
-	struct sigaction sa;
-	sa.sa_handler = handle_signal;
+	int i;
+	for (i = 0; i < 100; i++){								// Fill the char array with 0's, easier to convert key id for later
+		charArray[i] = 0;
+	}
+	
+	struct sigaction sa;									// declare sigaction
+	sa.sa_handler = handle_signal;							// tell which function sigaction calls
 	sa.sa_flags = 0;
 	sigemptyset(&sa.sa_mask);
 
-    if (sigaction(SIGINT, &sa, NULL) == -1) {
+    if (sigaction(SIGINT, &sa, NULL) == -1) {				// SIGINT is called
         perror("sigaction");
         exit(1);
     }
-    else if (sigaction(SIGUSR1, &sa, NULL) == -1){
+    else if (sigaction(SIGUSR1, &sa, NULL) == -1){			// SIGUSR1 is called
 		perror("sigaction");
 		exit(1);
 	}
@@ -53,7 +66,14 @@ int main() {
 	printf(" shmwriter: enter a shemloc: ");
 	fgets(charArray, 100, stdin);
 	printf("\n");
-	charArrToInt = atoi(charArray);							// ftok requires an int for id, convert the array to int for ftok use
+	// This loop is used to convert the charArray to an int. The int produced will be used
+	// to generate ftok key id for memory sharing.	
+	for(i = 0; i < 100; i++){
+		charArrToInt = (int)charArray[i] + charArrToInt;
+		printf("%d \n", charArrToInt);
+	}
+	
+	printf("%d\n", charArrToInt);
 	
 	key_t key = ftok("shmfile", charArrToInt);				//Generate a System V IPC Key for memshare
 
--- Operating_Systems/JShirleyP3/writer.c
+++ Operating_Systems/JShirleyP3/writer.c
@@ -1,3 +1,15 @@
+/*
+ * The purpose of this application is to write from a shared memory segment.
+ * This application will connect to a shared memory segment, and write to the
+ * contents of it. The user will define the shared memory segment to connect to.
+ * The application will prompt the user to input the shared memory segment, 
+ * and the application will connect to the shared memory segment according to
+ * the input from the user. The program will then prompt the user for input.
+ * The input will store in the shared memory segment. The program will close 
+ * upon ctl c input. The program will use SIGIT signal to catch ctl c. Upon
+ * call of SIGINT the memory will be detached and deleted.
+*/
+
 #include <stdio.h>
 #include <signal.h>
 #include <sys/msg.h>
@@ -10,7 +22,14 @@
 char* data;
 int shmid;
 
-void handle_sigint(int sig){
+
+/* handle_signal will handle signals called by the operating system.
+ * It will handle SIGINT and SIGUSR1. If SIGINT is called, the program
+ * will detach and clean up the shared memory segment as well as close the program. 
+ * If SIGUSR1 is called the program will state that SIGUSR1 was caught
+*/
+
+void handle_signal(int sig){
 	if (sig == SIGINT){
 		printf("\nshmwriter: SIGINT signal caught.\n");
 		printf("goodbye! thanks for sharing.\n");
@@ -28,20 +47,24 @@ void handle_sigint(int sig){
 }
 
 int main() {
-	void handle_sigint(int sig);
+	void handle_signal(int sig);					// define handle_signal function
 	char charArray[100];							//Used to identify shmloc
-	int charArrToInt;
-	struct sigaction sa;
+	int charArrToInt = 0;
+	int i;
+	for (i = 0; i < 100; i++){						// Fill array with 0's easier to convert key id later
+		charArray[i] = 0;
+	}
+	struct sigaction sa;							// declare sigaction
 	
-	sa.sa_handler = handle_sigint;
-	sa.sa_flags = SA_RESTART;
+	sa.sa_handler = handle_signal;					// tell which function sigaction to use
+	sa.sa_flags = 0;
 	sigemptyset(&sa.sa_mask);
 
-    if (sigaction(SIGINT, &sa, NULL) == -1) {
+    if (sigaction(SIGINT, &sa, NULL) == -1) {		// SIGINT was triggered
         perror("sigaction");
         exit(1);
     }
-    else if (sigaction(SIGUSR1, &sa, NULL) == -1){
+    else if (sigaction(SIGUSR1, &sa, NULL) == -1){	// SIGUSR1 was triggered
 		perror("sigaction");
 		exit(1);
 	}
@@ -49,6 +72,11 @@ int main() {
 	printf("%d shmwriter: enter a shemloc: ", getpid());
 	fgets(charArray, 100, stdin);
 	charArrToInt = atoi(charArray);
+	// This loop is used to convert the charArray to an int. The int produced will be used
+	// to generate ftok key id for memory sharing.
+	for(i = 0; i < 100; i++){
+		charArrToInt = (int)charArray[i] + charArrToInt;
+	}
 	
 	key_t key = ftok("shmfile", charArrToInt);				//Generate a System V IPC Key for memshare
 
@@ -57,7 +85,7 @@ int main() {
 		exit(1);
 	}
 	
-	shmid = shmget(key, sizeof(char)*100, 0644 | IPC_CREAT);
+	shmid = shmget(key, sizeof(char)*100, 0644 | IPC_CREAT);	// generate shared memory id
 	
 	if(shmid == -1){
 		perror("shmget");
